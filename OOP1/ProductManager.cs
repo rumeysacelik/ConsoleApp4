41496f867eac06365336bf5149d462b860518b37
rumeysacelik <rmysclk61@icloud.com>
2021-01-20 23:55:58 +0300

    20 ocak

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+
+        }
+    }
+
+    
+
+   
